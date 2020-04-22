1171a7e6a895a8fe386be5deb3b38626fee72e68
jocobtt <jacob.braswell00@byu.edu>
2020-04-21 20:58:37 -0400

    add dockerfile to python bank project etc.

--- python_prac/APIs/bnk/scrape/my_banks/wells.py
+++ python_prac/APIs/bnk/scrape/my_banks/wells.py
@@ -12,3 +12,33 @@ from bankscrap.scraper import unescape_html
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARN)
+
+
+class TLSv1Adapter(HTTPAdapter):
+    """Force TLSv1
+    Wells Fargo hangs with requests' default SSL setup so hard-set
+    TLSv1 here.
+    http://lukasa.co.uk/2013/01/Choosing_SSL_Version_In_Requests/
+    """
+
+    def init_poolmanager(self, connections, maxsize, **kwargs):
+        self.poolmanager = PoolManager(num_pools=connections,
+                                       maxsize=maxsize,
+                                       ssl_version=ssl.PROTOCOL_TLSv1)
+
+
+SCRAPER = requests.Session()
+SCRAPER.mount('https://', TLSv1Adapter())
+
+def login(user, pswrd):
+    SCRAPER.get('https://www.wellsfargo.com/')
+    login_data = {
+        'user': username,
+        'pswrd': password,
+        'screenid': 'SIGNON',
+        'origination': 'WebCons',
+        'LOB': 'Cons',
+    }
+    SCRAPER.post('https://online.wellsfargo.com/signon', login_data)
+    response = SCRAPER.get('https://online.wellsfargo.com/das/cgi-bin/session.cgi?screenid=SIGNON_PORTAL_PAUSE')
+    return response.text
