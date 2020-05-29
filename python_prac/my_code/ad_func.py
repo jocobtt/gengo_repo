d5f6d19b774452db330cfedeaf0644e4800a1806
jocobtt <jacob.braswell00@byu.edu>
2020-05-28 21:21:00 -0400

    hebi

---
+++ python_prac/my_code/ad_func.py
@@ -0,0 +1,15 @@
+# add function - playing around with python functions
+
+def add_stp(num1, *args):
+	sumz = num1
+	for x in args:
+		sumz = total + x
+	return sumz
+		
+
+
+def timz(num1, *args):
+	tot = num1
+	for x in args: 
+		tot = tot + x
+	return tot 
