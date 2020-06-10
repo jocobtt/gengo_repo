4b1e130d6cb5d026a6fe8c0bdef06bd54160373c
jocobtt <jacob.braswell00@byu.edu>
2020-06-09 21:45:07 -0400

    more work on scrap

--- python_prac/my_code/web_scp/scrape.py
+++ python_prac/my_code/web_scp/scrape.py
@@ -1,3 +1,13 @@
 import pandas as pd 
 import numpy as np 
 import helium 
+
+
+start_chrome('linkedin.com/login',headless=True)
+write('jacob.braswell@sas.com',into="Email or Phone")
+write(password.paz, into="Password")
+
+# get to page I want
+
+
+# scoll down 
