19261c4c2653bf8a1da23f45301feab4bdd5c7aa
jabras <jacob.braswell@sas.com>
2020-06-22 20:04:31 -0400

    more work

---
+++ python_prac/my_code/README.md
@@ -0,0 +1,4 @@
+Some random projects that I am doing in python for practice/fun
+
+https://towardsdatascience.com/how-to-build-a-simple-machine-learning-web-app-in-python-68a45a0e0291
+
--- python_prac/my_code/web_scp/scrape.py
+++ python_prac/my_code/web_scp/scrape.py
@@ -5,9 +5,13 @@ import helium
 
 start_chrome('linkedin.com/login',headless=True)
 write('jacob.braswell@sas.com',into="Email or Phone")
-write(password.paz, into="Password")
+write(password.paz, into="Password")  # find way to make this be more secure 
+click('Sign in')
 
 # get to page I want
+write("person", into="Search)
+press(ENTER)
 
+# click on person's page
 
 # scoll down 
