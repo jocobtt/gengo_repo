f9ee2ca4704fd15885fd6c9b5e65b0cd3f384081
jabras <jacob.braswell@sas.com>
2020-08-17 19:30:15 -0400

    more scrape

--- python_prac/my_code/web_scp/bs4-rent.py
+++ python_prac/my_code/web_scp/bs4-rent.py
@@ -20,9 +20,8 @@ URL = [
 "https://suumo.jp/chintai/tokyo/sc_nakano/"
 "https://suumo.jp/chintai/tokyo/sc_minato/"
 "https://suumo.jp/chintai/tokyo/sc_suginami/"
-""
-# other urls?  
 
+# other urls?  
 ]
 
 page = requests.get(URL)
@@ -31,15 +30,12 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 rent = soup.find_all('span', class_='cassetteitem_other-emphasis ui-text--bold')
 
-# info I need from apartment stuff - rent price, distance from places, general location/address, overall square feet, year made, story its at, rei amount, and name of the place
+# info I need from apartment stuff - rent price, distance from places, general location/address, overall square feet, year made, story its at, rei amount, and name of the place, ku_name
 
-"""loop through things version
-"""
 
-for b in URL:
-	html = requests.get(b).text
-	soup = BeautifulSoup(html, "html.parser")
-	# grab table we want to scrape
+
+
+# need to get the rest of these and then have it loop through all of the pages!!
 
 rent_price = []
 address = []
@@ -48,6 +44,11 @@ name_place = []
 rei_price = []
 distance = []
 apartment_type = []
+maintenence_price = []
+house_type = [] # cont.span
+year_built = []
+floor = []
+ku_name = []
 
 for rent in rent_elems:
 	rent_price = rent.find('span', class_='cassetteitem_other-emphasis ui-text--bold')
@@ -61,13 +62,46 @@ for rent in rent_elems:
 
 
 
+for container in apartment_container:
+	if container.find('div', class_ = "cassetteitem")
+
+	# the address 
+	addr = container.div.ul.li.text
+	address.append(addr)
+
+	# rent price 
+	renty = container.tbody.ul.span.text
+	rent_price.append(renty)
+
+	# sqr_m 
+	sqr = container.find('span', class_="cassetteitem_menseki").text # will have to clean this later
+	sqr_m.appen(sqr)
+
+	# name place 
+	name = container.div.li.text
+	name_place.append(name)
+
+	# rei_price 
+	rei = container.find('span', class_="cassetteitem_price cassetteitem_price--gratuity").text # will have to clean this later
+	rei_price.append(rei)
+
+	# distance?
+
+	# apartment_type
+	aprt = container.find("span", class_="cassetteitem_madori").text # will have to clean this later
+	apartment_type.append(apart)
+
+	# administration - span "cassetteitem_price cassetteitem_price--administration"
+
+
 df_ = pr.DataFrame({'rent_price': rent_price, 
 	'address' : address,
 	'sqr_m' : sqr_m,
 	'name_place' : name_place,
 	'rei_price' : rei_price,
 	'distance' : distance,
-	'apartment_type' : apartment_type})
+	'apartment_type' : apartment_type,
+	'ku_name' : ku_name})
 
 os.chdir('~/Downloads/')
 
