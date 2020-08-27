e7597ad0d90d80d75d83a66d474132e9e38395c8
jabras <jacob.braswell@sas.com>
2020-08-27 19:47:54 -0400

    more go

---
+++ learn_go/go_projects/packages-scopes/exec-flow.go
@@ -0,0 +1,19 @@
+package main
+
+import {
+	"fmt"
+}
+
+func main() {
+	fmt.Println("Hello!")
+
+	if 5 > 1 {
+		fmt.Println("bigger")
+
+	}
+}
+
+
+func main_2() {
+	fmt.print("hellow"); fmt.Println("world")
+}
---
+++ learn_go/go_projects/packages-scopes/importing.go
@@ -0,0 +1,12 @@
+package main
+
+
+import "fmt"
+import f "fmt"
+import fam "fmt"
+
+func main() {
+	fmt.Println("hello")
+	f.Println("hey")
+	fam.Println("hi")
+}
