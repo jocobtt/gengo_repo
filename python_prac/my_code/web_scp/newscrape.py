f695a6fe8a33184babbdf5ad0e033b318ae8e9ab
jabras <jacob.braswell@sas.com>
2020-09-08 21:48:08 -0400

    more scrape work

--- python_prac/my_code/web_scp/newscrape.py
+++ python_prac/my_code/web_scp/newscrape.py
@@ -20,8 +20,32 @@ subject = []
 date_written = []
 
 
+
+
+
 # figure out what websites to grab from - either asahi or https://www.sankei.com/
 
 # will have to loop from mainpage on each article and pull the text 
 # will have to be set with a cronjob 
 # then feed it to a runtime for analysis etc. 
+
+def art_scrape(num_articles):
+	print("starting process..")
+
+	art_text = []
+	author = []
+	title = []
+	subject = []
+	date_written = []
+	
+	URL = "https://www.asahi.com/"
+	
+	page = requests.get(url)
+
+	soup = BeautifulSoup(page.text, 'html.parser')
+	
+	articles = soup.find("div", class_="Main")
+
+	for article in articles:
+		article = 
+
