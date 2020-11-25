d060cf9f80953540825007d7563b0a3f38bea6cb
jabras <jacob.braswell@sas.com>
2020-11-25 14:21:13 -0500

    some scraping work

---
+++ python_prac/my_code/web_scp/navitime/navitime_scape.py
@@ -0,0 +1,29 @@
+import requests
+import re 
+import pandas as pd 
+from bs4 import BeautifulSoup
+import numpy as np 
+import os 
+from time import sleep
+
+navi = "https://www.navitime.co.jp/"
+POI_RE = re.compile(r"setPOI\(([^)]+)\);")
+
+ex_addr = "東京都中野区中野２"
+
+work_addr = "東京都港区六本木６"
+
+# want to pull long, lat, time to location, number of stops, bike time 
+
+# input search for location 
+
+from navitime import api
+
+
+def test_address_search():
+    results = api.address_search("六本木グランドタワー")
+    return results
+
+print(results)
+
+
